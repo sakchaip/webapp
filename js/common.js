f07716c51a21fc053e2ba36266c49817738f21b1
sakchaip <sakchai.pd@gmail.com>
2017-11-09 08:37:06 +0700

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,28 +1,39 @@
-var $g = {};
-
-$g.queryString = function () {
-  var qStr = {};
-  var query = window.location.search.substring(1); 
-  var vars = query.split("&"); 
-  for (var i=0;i<vars.length && query!='';i++) {
-	var pair = vars[i].split("=");
-		// If first entry with this name
-	if (typeof qStr[pair[0]] === "undefined") {
-	  qStr[pair[0]] = decodeURIComponent(pair[1]);
-		// If second entry with this name
-	} else if (typeof qStr[pair[0]] === "string") {
-	  var arr = [ qStr[pair[0]],decodeURIComponent(pair[1]) ];
-	  qStr[pair[0]] = arr;
-		// If third or later entry with this name
-	} else {
-	  qStr[pair[0]].push(decodeURIComponent(pair[1]));
-	}
-  } 
-    return qStr;
-}();
-
+//-- e-Analytics --\\
+$g={};
+$g.browser = (function(){
+    var ua= navigator.userAgent, tem,
+    M= ua.match(/(opera|chrome|safari|CriOS|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    } 
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!== null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!== null) M.splice(1, 1, tem[1]);
+    return {
+        "type":(M[0].replace('CriOS','chrome').toLowerCase()),
+        "ver":(M[1].toLowerCase()),
+        "mobile":(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)),
+        "touch":('ontouchstart' in window || window.DocumentTouch && document instanceof window.DocumentTouch || navigator.maxTouchPoints > 0 || window.navigator.msMaxTouchPoints > 0),
+        "svg":(document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Shape", "1.0") || false),
+        "canvas":(!!document.createElement("canvas").getContext || false)};
+})();
+$g.screen = {"width":screen.width,"height":screen.height};
+$g.display = {"width":window.innerWidth,"height":window.innerHeight};
+$g.element = function(id){{"width":document.getElementById(id).offsetWidth,"height":document.getElementById(id).innerHeight}};
 function importScript(url) {
     var script = document.createElement("script"); // Make a script DOM node
     script.src = url; // Set it's src to the provided URL
     document.head.appendChild(script); // Add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+};
+function moveElement(id,position){
+    var pos = (position || 'top-left');
+    var posTop = (pos.indexOf('middle')>-1)?(($g.display.height/2)-($g.element(id).height/2)).toString()+'px':(pos.indexOf('bottom')>-1)?($g.display.height-$g.element(id).height).toString()+'px':'0px');
+    var posLeft = ((pos.indexOf('center')>-1)?(($g.display.width/2)-($g.element(id).width/2)).toString()+'px':(pos.indexOf('right')>-1)?($g.display.width-$g.element(id).width).toString()+'px':'0px');
+	
+    document.getElementById(id).style.top=posTop;
+    document.getElementById(id).style.left=posLeft;
 }
